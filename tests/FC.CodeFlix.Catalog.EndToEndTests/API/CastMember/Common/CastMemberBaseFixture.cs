7c17d9ad815da7f7ebdcf4d07bc3c6a730472b4d
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2025-10-20 17:56:05 -0300

    CastMembers EndToEnd tests setup done.

---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/API/CastMember/Common/CastMemberBaseFixture.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using FC.CodeFlix.Catalog.Domain.Enum;
+using FC.CodeFlix.Catalog.EndToEndTests.Base;
+using DomainEntity = FC.CodeFlix.Catalog.Domain.Entity;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.API.CastMember.Common
+{
+    public class CastMemberBaseFixture : BaseFixture
+    {
+        public CastMemberPersistence Persistence { get; set; }
+
+        public CastMemberBaseFixture() : base()
+        {
+            Persistence = new CastMemberPersistence(CreateDbContext());
+        }
+
+        public List<DomainEntity.CastMember> GetExampleCastMembersList(int length = 10) => Enumerable.Range(1, length).Select(
+           _ => GetExampleCastMember())
+                   .ToList();
+
+        public DomainEntity.CastMember GetExampleCastMember()
+          => new DomainEntity.CastMember(
+              GetValidName(),
+              GetRandomCastMemberType()
+          );
+
+        public CastMemberType GetRandomCastMemberType()
+            => (CastMemberType)(new Random().Next(1, 2));
+
+        public string GetValidName()
+            => Faker.Name.FullName();
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/API/CastMember/Common/CastMemberPersistence.cs
@@ -0,0 +1,23 @@
+﻿using FC.Codeflix.Catalog.Infra.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using DomainEntity = FC.CodeFlix.Catalog.Domain.Entity;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.API.CastMember.Common
+{
+    public class CastMemberPersistence
+    {
+        private readonly CodeflixCatalogDbContext _context;
+
+        public CastMemberPersistence(CodeflixCatalogDbContext context)
+            => _context = context;
+
+        public async Task InsertList(List<DomainEntity.CastMember> castMembers)
+        {
+            await _context.CastMembers.AddRangeAsync(castMembers);
+            var test = await _context.SaveChangesAsync();
+        }
+
+        public async Task<DomainEntity.CastMember?> GetById(Guid id)
+            => await _context.CastMembers.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+    }
+}
