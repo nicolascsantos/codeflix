37d66a69015da84138433624d90af6c0237bbd34
Nicolas de Carvalho Santos <nicolas.santos@contrato.g4f.com.br>
2025-10-22 10:58:07 -0300

    string to rating extension created

---
+++ src/FC.CodeFlix.Catalog.Domain/Extensions/RatingExtension.cs
@@ -0,0 +1,20 @@
+﻿using FC.CodeFlix.Catalog.Domain.Enum;
+
+namespace FC.CodeFlix.Catalog.Domain.Extensions
+{
+    public static class RatingExtension
+    {
+        public static Rating ToRating(this string enumString)
+            => enumString switch
+            {
+                "ER" => Rating.ER,
+                "L" => Rating.L,
+                "10" => Rating.RATE_10,
+                "12" => Rating.RATE_12,
+                "14" => Rating.RATE_14,
+                "16" => Rating.RATE_16,
+                "18" => Rating.RATE_18,
+                _ => throw new ArgumentOutOfRangeException(nameof(enumString)),
+            };
+    }
+}
