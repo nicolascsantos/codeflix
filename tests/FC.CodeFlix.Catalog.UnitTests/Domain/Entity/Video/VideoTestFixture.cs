e126d8fa28dbda7ae1710e00c375f6806dec830e
Nicolas de Carvalho Santos <nicolas.santos@contrato.g4f.com.br>
2025-11-05 09:36:25 -0300

    creating videobasetestfixture

--- tests/FC.CodeFlix.Catalog.UnitTests/Domain/Entity/Video/VideoTestFixture.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Domain/Entity/Video/VideoTestFixture.cs
@@ -1,68 +1,11 @@
-﻿using FC.CodeFlix.Catalog.Domain.Enum;
-using FC.CodeFlix.Catalog.UnitTests.Common;
-using DomainEntity = FC.CodeFlix.Catalog.Domain.Entity;
+﻿using FC.CodeFlix.Catalog.UnitTests.Common.Fixtures;
 
 namespace FC.CodeFlix.Catalog.UnitTests.Domain.Entity.Video
 {
     [CollectionDefinition(nameof(VideoTestFixture))]
     public class VideoTestFixtureCollection : ICollectionFixture<VideoTestFixture> { }
 
-    public class VideoTestFixture : BaseFixture
+    public class VideoTestFixture : VideoTestFixtureBase
     {
-        public DomainEntity.Video GetValidVideo()
-            => new(GetValidTitle(), GetValidDescription(), GetValidYearLaunched(), true, true, GetValidDuration(), GetRandomRating());
-
-        public Rating GetRandomRating()
-        {
-            var enumValues = Enum.GetValues<Rating>();
-            var random = new Random();
-            return enumValues[random.Next(enumValues.Length)];
-        }
-
-        public string GetValidTitle()
-            => Faker.Lorem.Letter(100);
-
-        public string GetTooLongTitle()
-            => Faker.Lorem.Letter(400);
-
-        public string GetTooLongDescription()
-            => Faker.Lorem.Letter(4001);
-
-
-        public string GetValidDescription()
-            => Faker.Commerce.ProductDescription();
-
-        public int GetValidYearLaunched()
-            => Faker.Date.BetweenDateOnly
-            (
-                new DateOnly(1960, 1, 1),
-                new DateOnly(2025, 12, 31)
-            ).Year;
-
-        public int GetValidDuration()
-            => (new Random()).Next(100, 300);
-
-        public string GetValidImagePath()
-            => Faker.Image.PicsumUrl();
-
-        public string GetValidMediaPath()
-        {
-            var exampleMedias = new string[]
-            {
-                "www.googlestorage.com/file-example.mp4",
-                "www.storage.com/file-example.mp4",
-                "www.S3.com.br/file-example.mp4",
-                "www.glg.com/video.mp4",
-                "www.azure.com/example.mp4",
-            };
-
-            var random = new Random();
-
-            return exampleMedias[random.Next(exampleMedias.Length)];
-        }
-
-        public DomainEntity.Media GetValidMedia() 
-            => new(GetValidMediaPath());
-        
     }
 }
