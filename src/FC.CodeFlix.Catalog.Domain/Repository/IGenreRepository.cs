6e2465d58942b3c97e15ef15c19dffdc0897c001
Nicolas de Carvalho Santos <nicolas.carvalho@hotmail.com.br>
2025-11-07 09:38:27 -0300

    fix: changed entity name

--- src/FC.CodeFlix.Catalog.Domain/Repository/IGenreRepository.cs
+++ src/FC.CodeFlix.Catalog.Domain/Repository/IGenreRepository.cs
@@ -8,6 +8,6 @@ namespace FC.CodeFlix.Catalog.Domain.Repository
     {
         public Task<IReadOnlyList<Guid>> GetIdsListByIds(List<Guid> list, CancellationToken cancellationToken);
 
-        public Task<IReadOnlyList<Video>> GetListByIds(List<Guid> list, CancellationToken cancellationToken);
+        public Task<IReadOnlyList<Genre>> GetListByIds(List<Guid> list, CancellationToken cancellationToken);
     }
 }
