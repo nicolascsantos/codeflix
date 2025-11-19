a06d6e530c183c7a64f85988fbc2e3609408c6df
Nicolas de Carvalho Santos <nicolas.santos@contrato.g4f.com.br>
2025-11-19 11:09:55 -0300

    test: verification for when videos list is empty

--- tests/FC.CodeFlix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTest.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/Video/ListVideos/ListVideosTest.cs
@@ -6,6 +6,7 @@ using FC.CodeFlix.Catalog.Domain.SeedWork.SearchableRepository;
 using FluentAssertions;
 using Moq;
 using UseCases = FC.CodeFlix.Catalog.Application.UseCases.Video.ListVideos;
+using DomainEntity = FC.CodeFlix.Catalog.Domain.Entity;
 
 namespace FC.CodeFlix.Catalog.UnitTests.Application.Video.ListVideos
 {
@@ -74,6 +75,37 @@ namespace FC.CodeFlix.Catalog.UnitTests.Application.Video.ListVideos
                 outputItem.Genres.Should().BeEquivalentTo(exampleItem.Genres);
                 outputItem.Categories.Should().BeEquivalentTo(exampleItem.Categories);
             });
+            _videoRepositoryMock.VerifyAll();
+        }
+
+        [Fact(DisplayName = nameof(ListReturnsEmptyWhenThereIsNoVideo))]
+        [Trait("Application", "ListVideos - Use Cases")]
+        public async Task ListReturnsEmptyWhenThereIsNoVideo()
+        {
+            var input = new ListVideosInput(1, 10, "", "", SearchOrder.Asc);
+
+            _videoRepositoryMock.Setup(x => x.Search(
+                It.Is<SearchInput>(x =>
+                    x.Page == input.Page &&
+                    x.PerPage == input.PerPage &&
+                    x.Search == input.Search &&
+                    x.OrderBy == input.Sort &&
+                    x.Order == input.Dir),
+                It.IsAny<CancellationToken>()
+            )).ReturnsAsync(new SearchOutput<DomainEntity.Video>(
+                input.Page,
+                input.PerPage,
+                0,
+                new List<DomainEntity.Video>())
+            );
+
+            PaginatedListOutput<VideoModelOutput> output = await _useCase.Handle(input, CancellationToken.None);
+
+            output.Should().NotBeNull();
+            output.Page.Should().Be(input.Page);
+            output.PerPage.Should().Be(input.PerPage);
+            output.Items.Should().HaveCount(0);
+            output.Total.Should().Be(0);
             _videoRepositoryMock.VerifyAll();
         }
     }
