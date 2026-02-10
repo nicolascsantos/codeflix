f9a23be116861a04151dbca252acd50d5149ab3b
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-02-10 15:26:59 -0300

    feat: StorageService implementation done

--- src/FC.CodeFlix.Catalog.Infra.Storage/Services/StorageService.cs
+++ src/FC.CodeFlix.Catalog.Infra.Storage/Services/StorageService.cs
@@ -7,19 +7,29 @@ namespace FC.CodeFlix.Catalog.Infra.Storage.Services
 {
     public class StorageService : IStorageService
     {
+        private readonly StorageClient _storageClient;
+        private readonly StorageServiceOptions _options;
+
         public StorageService(StorageClient storageClient, IOptions<StorageServiceOptions> options)
         {
-            
+            _storageClient = storageClient;
+            _options = options.Value;
         }
 
-        public Task Delete(string filePath, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task Delete(string filePath, CancellationToken cancellationToken)
+            => await _storageClient.DeleteObjectAsync(_options.BucketName, filePath, cancellationToken: cancellationToken);
+        
 
-        public Task<string> Upload(string fileName, Stream fileStream, string contentType, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
+        public async Task<string> Upload(string fileName, Stream fileStream, string contentType, CancellationToken cancellationToken)
+        { 
+            var output = await _storageClient.UploadObjectAsync(
+                _options.BucketName,
+                fileName,
+                contentType,
+                fileStream,
+                cancellationToken: cancellationToken
+            );
+            return fileName;
         }
     }
 }
