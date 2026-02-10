18598e6e23b0a4ef4e9f7539f233753d238422f3
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-02-10 17:34:18 -0300

    test: raise event and clear events unit tests done

---
+++ tests/FC.CodeFlix.Catalog.UnitTests/Domain/SeedWork/AggregateRootTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Domain.SeedWork
+{
+    public class AggregateRootTest
+    {
+        [Fact(DisplayName = nameof(RaiseEvent))]
+        [Trait("Domain", "Aggregate Root")]
+        public void RaiseEvent()
+        {
+            var domainEvent = new DomainEventFake();
+            var aggregate = new AggregateRootFake();
+
+            aggregate.RaiseEvent(domainEvent);
+
+            aggregate.Events.Should().HaveCount(1);
+        }
+
+        [Fact(DisplayName = nameof(ClearEvents))]
+        [Trait("Domain", "Aggregate Root")]
+        public void ClearEvents()
+        {
+            var domainEvent = new DomainEventFake();
+            var aggregate = new AggregateRootFake();
+            aggregate.RaiseEvent(domainEvent);
+
+            aggregate.ClearEvent();
+
+            aggregate.Events.Should().HaveCount(0);
+        }
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.UnitTests/Domain/SeedWork/DomainEventFake.cs
@@ -0,0 +1,8 @@
+﻿using FC.CodeFlix.Catalog.Domain.SeedWork;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Domain.SeedWork
+{
+    public class DomainEventFake : DomainEvent
+    {
+    }
+}
