9c1e7d9303d62606eb3315874908616c44d08101
Nicolas de Carvalho <nicolas.carvalho@hotmail.com.br>
2026-03-03 19:11:07 -0300

    test: DomainEventPublisher unit test done

--- tests/FC.CodeFlix.Catalog.UnitTests/Application/DomainEventPublisherTest.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/DomainEventPublisherTest.cs
@@ -20,17 +20,17 @@ namespace FC.CodeFlix.Catalog.UnitTests.Application
             serviceCollection.AddSingleton(eventHandlerMock3.Object);
             var serviceProvider = serviceCollection.BuildServiceProvider();
             var domainEventPublisher = new DomainEventPublisher(serviceProvider);
-            var @event = new DomainEventToBeHandledFake();
+            DomainEvent @event = new DomainEventToBeHandledFake();
 
-            await domainEventPublisher.PublishAsync<DomainEventToBeHandledFake>(@event, CancellationToken.None);
+            await domainEventPublisher.PublishAsync<DomainEventToBeHandledFake>((dynamic)@event, CancellationToken.None);
 
             eventHandlerMock1.Verify(x => x.Handle(
-                @event,
+                (DomainEventToBeHandledFake)@event,
                 It.IsAny<CancellationToken>()
             ), Times.Once);
 
             eventHandlerMock2.Verify(x => x.Handle(
-                @event,
+                (DomainEventToBeHandledFake)@event,
                 It.IsAny<CancellationToken>()
             ), Times.Once);
 
@@ -45,21 +45,27 @@ namespace FC.CodeFlix.Catalog.UnitTests.Application
         public async Task NoActionWhenThereIsNoSubscriber()
         {
             var serviceCollection = new ServiceCollection();
-            var eventHandlerMock1 = new Mock<IDomainEventHandler<DomainEventToNotBeHandledFake>>();
-            var eventHandlerMock2 = new Mock<IDomainEventHandler<DomainEventToNotBeHandledFake>>();
+            var eventHandlerMock1 = new Mock<IDomainEventHandler<DomainEventToBeHandledFake>>();
+            var eventHandlerMock2 = new Mock<IDomainEventHandler<DomainEventToBeHandledFake>>();
+            var eventHandlerMock3 = new Mock<IDomainEventHandler<DomainEventToNotBeHandledFake>>();
             serviceCollection.AddSingleton(eventHandlerMock1.Object);
             serviceCollection.AddSingleton(eventHandlerMock2.Object);
+            serviceCollection.AddSingleton(eventHandlerMock3.Object);
             var serviceProvider = serviceCollection.BuildServiceProvider();
             var domainEventPublisher = new DomainEventPublisher(serviceProvider);
             var @event = new DomainEventToBeHandledFake();
 
-            await domainEventPublisher.PublishAsync< DomainEventToBeHandledFake>(@event, CancellationToken.None);
+            await domainEventPublisher.PublishAsync<DomainEventToBeHandledFake>(@event, CancellationToken.None);
 
             eventHandlerMock1.Verify(x => x.Handle(
-                It.IsAny<DomainEventToNotBeHandledFake>(),
+                @event,
                 It.IsAny<CancellationToken>()),
-            Times.Never);
+            Times.Once);
             eventHandlerMock2.Verify(x => x.Handle(
+                @event,
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+            eventHandlerMock3.Verify(x => x.Handle(
                 It.IsAny<DomainEventToNotBeHandledFake>(),
                 It.IsAny<CancellationToken>()),
             Times.Never);
